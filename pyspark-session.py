e42079dcea2f2a9acc156fac9462c6d91a0b377f
FranklinMartin <84481627+franklinmartin@users.noreply.github.com>
2024-03-19 14:55:44 -0400

    add equivalent frames

--- pyspark-session.py
+++ pyspark-session.py
@@ -23,3 +23,37 @@ df
 
 df.show()
 df.printSchema()
+
+df = spark.createDataFrame([
+    (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
+    (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
+    (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 0))
+], schema='a long, b double, c string, d date, e timestamp')
+df
+
+df.show()
+df.printSchema()
+
+pandas_df = pd.DataFrame({
+    'a': [1, 2, 3],
+    'b': [2., 3., 4.],
+    'c': ['string1', 'string2', 'string3'],
+    'd': [date(2000, 1, 1), date(2000, 2, 1), date(2000, 3, 1)],
+    'e': [datetime(2000, 1, 1, 12, 0), datetime(2000, 1, 2, 12, 0), datetime(2000, 1, 3, 12, 0)]
+})
+df = spark.createDataFrame(pandas_df)
+df
+
+df.show()
+df.printSchema()
+
+rdd = spark.sparkContext.parallelize([
+    (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
+    (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
+    (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 0))
+])
+df = spark.createDataFrame(rdd, schema=['a', 'b', 'c', 'd', 'e'])
+df
+
+df.show()
+df.printSchema()
